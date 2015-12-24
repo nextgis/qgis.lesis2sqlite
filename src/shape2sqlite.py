1b6e6165d261ea7defb246baca48ffcccf609ef1
alisovenko <alexander.lisovenko@gmail.com>
2015-12-24 17:35:22 +0300

    Add license text

--- src/shape2sqlite.py
+++ src/shape2sqlite.py
@@ -1,4 +1,32 @@
 # -*- coding: utf-8 -*-
+#******************************************************************************
+#
+# lesis2sqlite
+# ---------------------------------------------------------
+# This plugin convert lesis GIS working dir structure to sqlite data base
+#
+# Author:   Alexander Lisovenko, alexander.lisovenko@nextgis.ru
+# *****************************************************************************
+# Copyright (c) 2012-2015. NextGIS, info@nextgis.com
+#
+# This source is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# A copy of the GNU General Public License is available on the World Wide Web
+# at <http://www.gnu.org/licenses/>. You can also obtain it by writing
+# to the Free Software Foundation, 51 Franklin Street, Suite 500 Boston,
+# MA 02110-1335 USA.
+#
+#******************************************************************************
+
+
 import os
 import sys
 
